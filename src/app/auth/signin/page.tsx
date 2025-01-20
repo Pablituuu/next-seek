963b43255ba6d594cb32f250dead0356b99e5e88
pablituuu <keane.pa.si.in@gmail.com>
2025-01-20 13:35:26 -0500

    feat: add shacdn

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
