245d827982d79ba1ef9b281e527416cb22b2e175
pablituuu <keane.pa.si.in@gmail.com>
2025-01-21 10:35:38 -0500

    chore: debugging in prod

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import TaskList from "@/components/dashboard/task-list";
 import CreateTask from "@/components/dashboard/create-task";
 import UserMenu from "@/components/dashboard/user-menu";
+import { getTasks } from "@/service/server";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -9,8 +10,8 @@ async function Page() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
   if (!token) redirect("/auth/signin");
-  // const tasks = await getTasks();
-
+  const tasks = await getTasks();
+  console.log(tasks);
   return (
     <div className="container mx-auto p-4 flex flex-col items-center">
       <ScrollArea className="w-full h-[90vh] border border-gray-200">
