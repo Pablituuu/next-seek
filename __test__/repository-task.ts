beea19a3e85b71b170029b2fb9334110c910971e
pablituuu <keane.pa.si.in@gmail.com>
2025-01-21 10:31:03 -0500

    chore: remove unnecessary code

--- __test__/repository-task.ts
+++
@@ -1,8 +0,0 @@
-import { getTasks } from "@/service/server";
-
-describe("Repository Task", () => {
-    it("should return a list of tasks", async () => {
-        const tasks = await getTasks();
-        expect(tasks.lenght).toBeGreaterThanOrEqual(0)
-    });
-});
