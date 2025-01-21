659d1ef7857e337e97e03e5311c156c7509af89c
pablituuu <keane.pa.si.in@gmail.com>
2025-01-21 11:12:58 -0500

    chore: debugging in prod

--- src/service/server.ts
+++ src/service/server.ts
@@ -3,9 +3,10 @@ import { headers } from "next/headers";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getTasks = async () => {
+    const header = new Headers(await headers())
     const response = await fetch(`${API_URL}/task`, {
         method: "GET",
-        headers: await headers(),
+        headers: header,
     });
     const data = await response.json();
     return data;
